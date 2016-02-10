f6ac6fc8369e4cfd8247f658af2acd7ccc6cf34b
LightningStairs <jenah.blitz@icloud.com>
2016-02-10 17:39:11 -0500

    Updated Responsive Design
    Updated Nav on index page and fixed some problems on the Zombie JR
    page. Also added Other Artwork page.

--- BD_Code_Files/js/scripts.js
+++ BD_Code_Files/js/scripts.js
@@ -3,9 +3,22 @@ $(document).ready(function(){
 		infinite: true,
 		speed: 500,
 		fade: true,
+		accessibility: true,
 		cssEase: 'linear',
-	
-		
 
+		responsive: [
+    {
+      breakpoint: 925,
+      settings: {
+        slidesToShow: 1,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        accessibility: true,
+        mobileFirst: true,
+        touchMove: true,
+        respondTo: window,
+      }
+    },
+  ]
 	});
 });
